cdd466fca3a41d57ae5faa6fbc74e0621bc3cac7
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-16 01:55:56 +0200

    test: better integration test

--- venty/aggregate_store_test.py
+++ venty/aggregate_store_test.py
@@ -9,6 +9,7 @@ from venty.in_memory_event_store import InMemoryEventStore
 
 class Book(DataclassEntity):
     name: Optional[str]
+    checked_out_by: Optional[str]
 
     @property
     def entity_id(self) -> EntityId:
@@ -18,22 +19,43 @@ class Book(DataclassEntity):
     def when(self, event: CloudEvent) -> None:
         if event.get("type") == "book-created":
             self.name = event.get("subject")
+        if event.get("type") == "book-checked-out":
+            self.checked_out_by = event.get("source")
+        if event.get("type") == "book-returned":
+            self.checked_out_by = None
 
     @classmethod
     def create(cls, name: str):
         event = CloudEvent.create(
-            {"type": "book-created", "subject": name, "source": "dummy"}, None
+            {"type": "book-created", "subject": name, "source": "library"}, None
         )
         result = cls()
         result.apply(event)
         return result
 
+    def check_out(self, patron: str):
+        self.apply(
+            CloudEvent.create({"type": "book-checked-out", "source": patron}, None)
+        )
 
-def test_store_load():
-    hello_world = EntityId("hello-world")
-    original_book = Book.create(hello_world)
-    books = AggregateStore(InMemoryEventStore())
-    books.store(original_book)
+    def return_book(self):
+        self.apply(
+            CloudEvent.create({"type": "book-returned", "source": "any-patron"}, None)
+        )
 
-    loaded_book = books.load(Book, hello_world)
+
+def test_store_integration():
+    the_idiot = EntityId("The Idiot")
+    original_book = Book.create(the_idiot)
+    library = AggregateStore(InMemoryEventStore())
+    library.store(original_book)
+
+    loaded_book = library.load(Book, the_idiot)
     assert loaded_book.name == original_book.name
+
+    loaded_book.check_out("Alice")
+    library.store(loaded_book)
+
+    original_book = library.fetch(original_book)
+
+    assert original_book.checked_out_by == "Alice"
