e3c83d4c64ff35365827d30c1ae850d96dd246c6
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-16 01:37:53 +0200

    build: drop python 3.7 support

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ if __name__ == "__main__":
             "Development Status :: 5 - Production/Stable",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
