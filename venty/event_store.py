c4c2b8ad513fc8d9e235ff58e5f53ba901c2ab6e
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-15 20:28:34 +0200

    refactor: better interface

--- venty/event_store.py
+++ venty/event_store.py
@@ -25,10 +25,28 @@ class EventStore:
         self,
         stream_name: StreamName,
         *,
-        current_version: StreamVersion,
+        expected_version: StreamVersion,
         events: Iterable[CloudEvent],
         timeout: Optional[timedelta] = None,
-    ) -> CommitPosition:
+    ) -> Optional[CommitPosition]:
+        """
+        :param stream_name: unique identifier of a stream inside this event store.
+            The stream name is unique only in the context of this event store.
+            You MUST NOT assume it is globally unique.
+        :param expected_version: The version of the stream before the events were
+            committed to it, if the actual version of the stream does not match this
+            value the events will not be appended to it and no commit position will
+            be returned from this function.
+        :param events: events which should be committed to the stream at the given
+            version.
+        :param timeout: once this duration passed from the start of the execution
+            this function MAY raise TimeoutError.
+            If the TimeoutError exception was raised, events MUST NOT be committed to
+            the stream.
+        :return: If the append operation succeeded will return the event store global
+            commit position of the events.
+            If the expected version was wrong, None MUST be returned.
+        """
         raise NotImplementedError()
 
     def read_streams(
@@ -39,6 +57,9 @@ class EventStore:
         limit: int = sys.maxsize,
         timeout: Optional[timedelta] = None,
     ) -> Iterable[RecordedEvent]:
+        raise NotImplementedError()
+
+    def commit_position(self) -> CommitPosition:
         raise NotImplementedError()
 
 
@@ -55,7 +76,7 @@ def append_event(
     """
     return repo.append_events(
         stream_name=stream_name,
-        current_version=current_version,
+        expected_version=current_version,
         events=(event,),
         timeout=timeout,
     )
@@ -74,7 +95,7 @@ def append_events(
     """
     return repo.append_events(
         stream_name=stream_name,
-        current_version=current_version,
+        expected_version=current_version,
         events=events,
         timeout=timeout,
     )
