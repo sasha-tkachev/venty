dc8eba67829f488c0e5bca304c7683c8789ad59b
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-15 22:37:18 +0200

    test: add deterministic type

--- venty/strong_types_test.py
+++ venty/strong_types_test.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 from uuid import UUID, uuid5
-from datetime import timedelta
+from datetime import timedelta, datetime, timezone
 from venty.strong_types import StreamName, EventType
 from cloudevents.pydantic.v2 import CloudEvent
 import time
@@ -16,6 +16,7 @@ def dummy_events(
     source: str = "dummy-source",
     seed: UUID = UUID(int=1),
     interval: timedelta = timedelta(seconds=0),
+    start_time: datetime = datetime(year=2024, month=1, day=1, tzinfo=timezone.utc),
 ) -> Iterable[CloudEvent]:
     for i in range(limit):
         time.sleep(interval.total_seconds())
@@ -24,5 +25,6 @@ def dummy_events(
                 "type": type_,
                 "source": source,
                 "id": str(uuid5(namespace=seed, name=str(i))),
+                "time": (start_time + timedelta(seconds=i)).isoformat(),
             }
         )
