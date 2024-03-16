12b149124faca2c89d39b2b9e9d4d3bda2c7b2a8
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-16 03:45:30 +0200

    feat: add import functions

--- venty/__init__.py
+++ venty/__init__.py
@@ -1,1 +1,9 @@
-from venty.event_store import EventStore
+from venty.event_store import (
+    EventStore,
+    read_stream_no_metadata,
+    read_stream,
+    attempt_append_event,
+    attempt_append_events,
+    append_events,
+    append_event,
+)
