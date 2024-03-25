05bd4a9b6680e7e06df7934ec621fc284293140e
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-26 01:23:05 +0200

    build: bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 if __name__ == "__main__":
     setuptools.setup(
         name="venty",
-        version="2.0.0",
+        version="3.0.0",
         author="Alexander Tkachev",
         author_email="sasha64sasha@gmail.com",
         description="Venty",
