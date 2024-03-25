0fb83189c8f7e30bb5afeed65b823e7d7bce42bb
Alexander Tkachev <sasha64sasha@gmail.com>
2024-03-26 01:55:44 +0200

    fix: commit on stream creation

--- venty/sql_event_store.py
+++ venty/sql_event_store.py
@@ -206,6 +206,7 @@ def _commit_append_events(
         return None
     if stream_id is None:
         stream_id = _create_stream(stream_name, session)
+        session.commit()
 
     row_records = _record_event_rows(
         events=events,
@@ -241,7 +242,7 @@ class SqlEventStore(EventStore):
                     return _commit_append_events(
                         stream_name, expected_version, consumed_events, session
                     )
-                except IntegrityError:
+                except IntegrityError as e:
                     session.rollback()
 
     def read_streams(
