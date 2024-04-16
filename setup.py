455a5bbd32777dea7cf3c5f7922d230e3ab83a32
Alexander Tkachev <sasha64sasha@gmail.com>
2024-04-16 23:20:36 +0300

    build: bump version
    BREAKING CHANGE

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 if __name__ == "__main__":
     setuptools.setup(
         name="venty",
-        version="7.1.0",
+        version="8.0.0",
         author="Sasha Tkachev",
         author_email="sasha64sasha@gmail.com",
         description="Venty",
